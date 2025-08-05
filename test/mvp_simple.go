979822b4fcf1f1c60ac658405f98143fee2d1f19
Rafael Mori <faelmori@gmail.com>
2025-08-05 01:35:26 -0300

    feat: adicionar exemplos de configuração de teste com modos Debug e Verbose
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- test/mvp_simple.go
+++ test/mvp_simple.go
@@ -2,22 +2,21 @@ package main
 
 import "fmt"
 
-// Simple test struct
-type Config struct {
+type TestConfig struct {
 	Debug   bool
 	Verbose bool
 }
 
 func main() {
-	cfg := Config{Debug: true, Verbose: false}
-	
+	cfg := TestConfig{Debug: true, Verbose: false}
+
 	if cfg.Debug {
 		fmt.Println("Debug mode enabled")
 	}
-	
+
 	if cfg.Verbose {
-		fmt.Println("Verbose mode enabled") 
+		fmt.Println("Verbose mode enabled")
 	}
-	
+
 	fmt.Printf("Config: Debug=%t, Verbose=%t\n", cfg.Debug, cfg.Verbose)
 }
